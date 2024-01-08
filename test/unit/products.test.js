3a94f3fc4d094fa5473eefb2433bdaaa3dd578da
yujuck <yujin3278@gmail.com>
2024-01-08 21:40:00 +0900

    결과값 전달 테스트

--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -33,4 +33,10 @@ describe("Product Controller Create", () => {
     expect(res.statusCode).toBe(201);
     expect(res._isEndCalled()).toBeTruthy(); // _isEndCalled()는 res.send()가 호출되었는지를 확인하는 함수 (node-mocks-http의 기능)
   });
+
+  it("should return json body in response", () => {
+    productModel.create.mockReturnValue(newProduct);
+    productController.createProduct(req, res, next);
+    expect(res._getJSONData()).toStrictEqual(newProduct); // _getJSONData()는 전달된 데이터를 확인하는 함수 (node-mocks-http의 기능)
+  });
 });
