af04061da773200e3a0755f1f74c13ab8669f9a1
yujuck <yujin3278@gmail.com>
2024-01-09 22:45:41 +0900

    async await 적용

--- controller/product.js
+++ controller/product.js
@@ -1,6 +1,6 @@
 const productModel = require("../models/Product");
 
-exports.createProduct = (req, res, next) => {
-  const createProduct = productModel.create(req.body);
+exports.createProduct = async (req, res, next) => {
+  const createProduct = await productModel.create(req.body);
   res.status(201).json(createProduct);
 };
