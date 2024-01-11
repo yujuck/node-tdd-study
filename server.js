9e852391144bc1d272031fb9293f218c2aec6e5b
yujuck <yujin3278@gmail.com>
2024-01-11 22:53:24 +0900

    통합 테스트

--- server.js
+++ server.js
@@ -16,5 +16,11 @@ app.get('/', (req, res) => {
   res.send("Hello");
 })
 
+app.use((error, req, res, next) => {
+  res.status(500).json({message: error.message});
+});
+
 app.listen(PORT);
 console.log(`Running on port ${PORT}`);
+
+module.exports = app;
---
+++ test/integration/products.int.test.js
@@ -0,0 +1,24 @@
+// 통합 테스트에서는 데이터베이스 부분도 mock으로 하지 않고 실제 데이터베이스를 사용한다.
+
+const request = require('supertest');
+const app = require('../../server');
+const newProduct = require('../data/new-product.json');
+
+it("POST /api/products", async () => {
+  const response = await request(app)
+    .post("/api/products")
+    .send(newProduct);
+
+  expect(response.statusCode).toBe(201);
+  expect(response.body.name).toBe(newProduct.name);
+  expect(response.body.description).toBe(newProduct.description)
+});
+
+it("should return 500 on POST /api/products", async () => {
+  const response = await request(app)
+    .post("/api/products")
+    .send({name: "phone"});
+  
+    expect(response.statusCode).toBe(500);
+    expect(response.body).toStrictEqual({message: "Product validation failed: description: Path `description` is required."})
+});
